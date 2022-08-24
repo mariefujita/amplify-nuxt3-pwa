5fa956016cee7809cc9d48fa43e61b9ea223f411
Marie Fujita <mariefujita94@gmail.com>
2022-08-24 09:09:57 +0900

    fix bug

--- src/plugins/amplify.ts
+++ src/plugins/amplify.ts
@@ -1,6 +1,6 @@
 import { defineNuxtPlugin } from '#app'
 import awsExports from '../aws-exports'
-import { Amplify } from 'aws-amplify'
+import { Amplify } from '@aws-amplify/core'
 
 export default defineNuxtPlugin(() => {
   Amplify.configure({ ...awsExports, ssr: true })
