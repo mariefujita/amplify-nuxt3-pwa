33bd53937acc8b911545fcf537615378cb74a9ff
Marie Fujita <mariefujita94@gmail.com>
2022-09-05 18:09:46 +0900

    amplify config for vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,27 @@ export default defineNuxtConfig({
   server: {
     port: 3000,
   },
+  publicRuntimeConfig: {
+    region: process.env.REGION,
+    cognitoIdentityUserPoolId:
+      process.env.NODE_ENV !== 'production' ? process.env.COGNITO_IDENTITY_USER_POOL : undefined,
+    userPoolId: process.env.NODE_ENV !== 'production' ? process.env.USER_POOLS_ID : undefined,
+    userPoolWebClientId: process.env.NODE_ENV !== 'production' ? process.env.USER_POOLS_WEB_CLIENT_ID : undefined,
+    cognitoAttr: process.env.COGNITO_ATTRIBUTE,
+    cognitoMfaConf: process.env.COGNITO_MFA_CONF,
+    cognitoMfaType: process.env.COGNITO_MFA_TYPE,
+    cognitoPasswordLength: process.env.COGNITO_PASSWORD_LENGTH,
+    appSyncEndPoint: process.env.NODE_ENV !== 'production' ? process.env.APPSYNC_END_POINT : undefined,
+    appSyncAuthType: process.env.APPSYNC_AUTH_TYPE,
+    appSyncApiKey: process.env.NODE_ENV !== 'production' ? process.env.APPSYNC_API_KEY : undefined,
+  },
+  privateRuntimeConfig: {
+    appSyncApiKey: process.env.APPSYNC_API_KEY,
+    cognitoIdentityUserPoolId: process.env.COGNITO_IDENTITY_USER_POOL,
+    userPoolId: process.env.USER_POOLS_ID,
+    userPoolWebClientId: process.env.USER_POOLS_WEB_CLIENT_ID,
+    appSyncEndPoint: process.env.APPSYNC_END_POINT,
+  },
   modules: ['@nuxtjs/color-mode', '@kevinmarrec/nuxt-pwa'],
   postcss: {
     plugins: {
