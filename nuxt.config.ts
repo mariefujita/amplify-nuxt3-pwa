d7f7b9b7b7c41fc1d91c4e1fbe090dfa90d598c8
Marie Fujita <mariefujita94@gmail.com>
2022-09-06 15:38:01 +0900

    remove runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,27 +7,6 @@ export default defineNuxtConfig({
   server: {
     port: 3000,
   },
-  publicRuntimeConfig: {
-    region: process.env.REGION,
-    cognitoIdentityUserPoolId:
-      process.env.NODE_ENV !== 'production' ? process.env.COGNITO_IDENTITY_USER_POOL : undefined,
-    userPoolId: process.env.NODE_ENV !== 'production' ? process.env.USER_POOLS_ID : undefined,
-    userPoolWebClientId: process.env.NODE_ENV !== 'production' ? process.env.USER_POOLS_WEB_CLIENT_ID : undefined,
-    cognitoAttr: process.env.COGNITO_ATTRIBUTE,
-    cognitoMfaConf: process.env.COGNITO_MFA_CONF,
-    cognitoMfaType: process.env.COGNITO_MFA_TYPE,
-    cognitoPasswordLength: process.env.COGNITO_PASSWORD_LENGTH,
-    appSyncEndPoint: process.env.NODE_ENV !== 'production' ? process.env.APPSYNC_END_POINT : undefined,
-    appSyncAuthType: process.env.APPSYNC_AUTH_TYPE,
-    appSyncApiKey: process.env.NODE_ENV !== 'production' ? process.env.APPSYNC_API_KEY : undefined,
-  },
-  privateRuntimeConfig: {
-    appSyncApiKey: process.env.APPSYNC_API_KEY,
-    cognitoIdentityUserPoolId: process.env.COGNITO_IDENTITY_USER_POOL,
-    userPoolId: process.env.USER_POOLS_ID,
-    userPoolWebClientId: process.env.USER_POOLS_WEB_CLIENT_ID,
-    appSyncEndPoint: process.env.APPSYNC_END_POINT,
-  },
   modules: ['@nuxtjs/color-mode', '@kevinmarrec/nuxt-pwa'],
   postcss: {
     plugins: {
@@ -46,7 +25,7 @@ export default defineNuxtConfig({
     define: {
       'process.env.DEBUG': false,
       // https://github.com/nuxt/framework/issues/4916
-      'window.global': {}, // ← yarn dev するときはコメントアウトを外す
+      // 'window.global': {}, // ← yarn dev するときはコメントアウトを外す
     },
     resolve: {
       alias: {
--- src/plugins/amplify.ts
+++ src/plugins/amplify.ts
@@ -1,30 +1,7 @@
 import { defineNuxtPlugin } from '#app'
-import { useRuntimeConfig } from '#imports'
 import awsExports from '../aws-exports'
 import { Amplify } from '@aws-amplify/core'
 
 export default defineNuxtPlugin(() => {
-  const config = useRuntimeConfig()
-  Amplify.configure({
-    aws_project_region: config.region,
-    aws_cognito_identity_pool_id: config.cognitoIdentityUserPoolId,
-    aws_cognito_region: config.region,
-    aws_user_pools_id: config.userPoolId,
-    aws_user_pools_web_client_id: config.userPoolWebClientId,
-    oauth: {},
-    aws_cognito_username_attributes: [config.cognitoAttr],
-    aws_cognito_social_providers: [],
-    aws_cognito_signup_attributes: [config.cognitoAttr],
-    aws_cognito_mfa_configuration: config.cognitoMfaConf,
-    aws_cognito_mfa_types: [config.cognitoMfaType],
-    aws_cognito_password_protection_settings: {
-      passwordPolicyMinLength: config.cognitoPasswordLength,
-      passwordPolicyCharacters: [],
-    },
-    aws_cognito_verification_mechanisms: [config.cognitoAttr],
-    aws_appsync_graphqlEndpoint: config.appSyncEndPoint,
-    aws_appsync_region: config.region,
-    aws_appsync_authenticationType: 'API_KEY',
-    aws_appsync_apiKey: config.appSyncApiKey,
-  })
+  Amplify.configure({ ...awsExports })
 })
